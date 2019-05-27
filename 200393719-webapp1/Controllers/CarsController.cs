dce5e80948bcd8fca40a57a3a8873396068b2d02
Bik097 <bjs241997@gmail.com>
2019-05-27 18:01:54 -0400

    3

--- 200393719-webapp1/Controllers/CarsController.cs
+++ 200393719-webapp1/Controllers/CarsController.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using _200393719_webapp1.Models;
 
 namespace _200393719_webapp1.Controllers
 {
     public class CarsController : Controller
     {
+
+        List<Cars> carList = new List<Cars>()
+        {
+        new Cars { Id = 1, Brand = "Toyota", Model = "Corolla", Type = "Compact", Year = 2017},
+        new Cars { Id = 2, Brand = "Ford", Model = "F-150", Type = "Truck", Year = 2018},
+        new Cars { Id = 3, Brand = "Dodge", Model = "Caravan", Type = "Minivan", Year = 2019}
+        };
         // GET: Cars
         public ActionResult Index()
         {
-            return View();
+            //var content = "/Cars - this is a list of cars from Content()";
+            return View(carList);
         }
+
+        //Get:Cars/Details - Single Car
+        public ActionResult Details(int? id)
+        {
+            if (id == null || id > carList.Count)
+            {
+                return Content("Invalid car id");
+            }
+            var ind = Convert.ToInt32(id) - 1;
+            var car = car = carList[ind];
+
+            return View(car);
+
+}
     }
 }
+
---
+++ 200393719-webapp1/Models/Cars.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _200393719_webapp1.Models
+{
+    public class Cars
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Type { get; set; }
+        public int Year { get; set; }
+    }
+}
